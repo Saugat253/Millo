019f20cff2f8bcc65c223511b40af2d3dac7f120
ashish khatiwada <ashishkhatiwada212@gmail.com>
2020-08-17 16:58:09 +0545

    Setup Props of User Table.

--- Millo/Models/User.cs
+++ Millo/Models/User.cs
@@ -8,8 +8,17 @@ using System.Web;
 
 namespace Millo.Models
 {
-    public class User : DbContext
+    public class User 
     {
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            this.UserClaims = new HashSet<UserClaim>();
+            this.UserLogins = new HashSet<UserLogin>();
+            this.Roles = new HashSet<Role>();
+        }
+
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
@@ -19,6 +28,25 @@ namespace Millo.Models
         public string LastName { get; set; }
         public string FullAddress { get; set; }
 
+       
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public Nullable<System.DateTime> LockoutEndDateUtc { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+        
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<UserClaim> UserClaims { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<UserLogin> UserLogins { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Role> Roles { get; set; }
         [Phone]
         public int  Phone { get; set; }
 
@@ -31,8 +59,9 @@ namespace Millo.Models
 
 
 
+
     }
-    public class ProfilePic : DbContext
+    public class ProfilePic 
     {
         public int ProfilePicId { get; set; }
         public string Location { get; set; }
@@ -44,4 +73,52 @@ namespace Millo.Models
         public int Wow { get; set; }
         public int Comments { get; set; }
     }
+
+
+
+
+   
+    public partial class Role
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Role()
+        {
+            this.Users = new HashSet<User>();
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<User> Users { get; set; }
+    }
+
+
+
+
+
+    public partial class UserClaim
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+
+        public virtual User User { get; set; }
+    }
+
+
+
+
+
+    public partial class UserLogin
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
 }
+
+
